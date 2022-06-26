63e29c374aa973aeec2df75b21af56106f34f44c
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2022-06-25 23:04:55 -0300

    Consuming data from graphcms.

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,15 +1,64 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
+import { gql, useQuery } from "@apollo/client";
+
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
 
 import '@vime/core/themes/default.css'
 
-export function Video() {
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug($slug: String) {
+  lesson(where: {slug: $slug}) {
+    videoId
+    title
+    id
+    description
+    teacher {
+      name
+      bio
+      avatarURL
+    }
+  }
+}`
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string,
+    videoId: string
+    description: string
+    teacher: {
+      bio: string
+      avatarURL: string
+      name: string
+    }
+  }
+}
+
+interface videoProps {
+  lessonSlug: string
+}
+
+export function Video(props: videoProps) {
+
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: props.lessonSlug
+    }
+  })
+
+  if(!data) {
+    return(
+      <div className="flex-1">
+        <p>Carregando...</p>
+      </div>
+    )
+  }
+
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
         <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
           <Player>
-            <Youtube videoId={"hk84hLiO8DU"} />
+            <Youtube videoId={data.lesson.videoId} />
             <DefaultUi />
           </Player>
         </div>
@@ -19,22 +68,22 @@ export function Video() {
         <div className="flex items-start gap-16">
           <div className="flex-1">
             <h1 className="text-2xl font-bold">
-              Introdução ao ASP.NET
+              {data.lesson.title}
             </h1>
             <p className="mt-4 text-gray-200 leading-relaxed">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer mattis vulputate aliquam. Curabitur ante neque, vehicula quis erat sed, elementum finibus turpis. Maecenas a massa sapien.
+              {data.lesson.description}
             </p>
 
             <div className="flex items-center gap-4 mt-6">
               <img 
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src="https://avatars.githubusercontent.com/u/78623408?v=4"
+                src={data.lesson.teacher.avatarURL}
                 alt="Teacher image" 
               />
 
               <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">Guilherme Azevedo</strong>
-                <span className="text-gray-200 text-sm block">Fullstack developer</span>
+                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
               </div>
             </div>
           </div>
