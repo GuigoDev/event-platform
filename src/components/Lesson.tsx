800ecfc91d70b0f915188c33e82effb95df5f270
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2022-06-25 22:02:37 -0300

    Lesson hover.

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -17,12 +17,12 @@ export function Lesson(props: LessonProps) {
   })
 
   return (
-    <a href="#" className="text-gray-300">
-      <span> 
+    <a href="#" className="group">
+      <span className="text-gray-300"> 
         { availableDateFormatted } 
       </span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className="flex items-center justify-between">
           
           { isLessonAvailable ? (
