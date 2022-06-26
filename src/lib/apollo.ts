f2cea658d0a1bc67841dcfe1db1159e5c403a77e
Guilherme Azevedo <guilhermeazevedo.deb@gmail.com>
2022-06-26 08:24:37 -0300

    Auth token.

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oir0st062z01xtej879ogp/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
